2ce507ed0c1bf9f93e31faf87ae50504fa12bb20
rangho.park <rangho.park@wayties.com>
2025-12-02 18:28:46 +0900

    init.

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.wayties.test_cicd"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.wayties.test_cicd"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        dataBinding = true  // MVVM 패턴 사용 시 필수
+        // viewBinding = true  // ViewBinding만 사용할 경우
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.github.Rhpark.Simple_UI_XML:Simple_UI_Core:0.3.13")  // Core functionality only.
+    implementation("com.github.Rhpark.Simple_UI_XML:Simple_UI_XML:0.3.13")   // XML UI components (includes Core).
+}
---
+++ app/src/main/java/com/wayties/test_cicd/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.wayties.test_cicd
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kr.open.library.simple_ui.xml.ui.activity.BaseActivity
+
+class MainActivity : BaseActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}

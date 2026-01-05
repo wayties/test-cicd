2ddc2623157891088dec0688ab3591b66cb1de97
rangho.park <rangho.park@wayties.com>
2026-01-05 17:06:35 +0900

    Dooray 연동 테스트를 위한 주석 추가.

--- app/src/main/java/com/wayties/test_cicd/MainActivity.kt
+++ app/src/main/java/com/wayties/test_cicd/MainActivity.kt
@@ -5,6 +5,9 @@ import android.widget.Button
 import com.wayties.library.toastShowShort1
 import kr.open.library.simple_ui.xml.ui.activity.BaseActivity
 
+/**
+ * 주석 추가  -> Github Push 에서 Dooray 메신저 알람을 위한 테스트 주석
+ */
 class MainActivity : BaseActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
